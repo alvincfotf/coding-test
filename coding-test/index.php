0541e117e71a88ebd4792aee3a3b562721563aae
alvincfotf <77079907+alvincfotf@users.noreply.github.com>
2021-12-03 10:42:18 -0800

    Update index.php

--- coding-test/index.php
+++ coding-test/index.php
@@ -22,7 +22,7 @@ require_once 'config.php';
     <p>We would like to be able to scrape broadcasts off our radio site and store some information about it into a database. We would also be able to query and display broadcasts from the database based on a specific date range.</p>
 
     <h2>Scrape Focus Canada Broadcasts</h2>
-    <p>The page at https://radio.focusonthefamily.ca/schedule/broadcasts/MM-YY contains a table that has all currently uploaded broadcasts for that month.</p>
+    <p>The page at https://radio.focusonthefamily.ca/schedule/broadcasts/YYYY-MM contains a table that has all currently uploaded broadcasts for that month.</p>
     <p>Your job is to create a scraping function in PHP that will scrape that page for the date, the title, and link for every broadcast. Store broadcasts with their Date, Link and Title and Guests in a MySQL database. The function should also follow the link to the Guest page and scrape the Description field as well, and then store the Guest with their Name, Link and Description to another table in the database. This function should be called with an asynchronous call to the scrape.php route in scrape.js. The provided select field (feel free to modify it) will be sent to your Javascript and the value should be sent along to scrape.php to construct the corresponding URL for that month's broadcasts. You would also have to create a separate route in search.php, called by a separate form in order to display all stored broadcasts in the database in a specific date range. </p>
     <h2>Requirements</h2>
     <ul>
